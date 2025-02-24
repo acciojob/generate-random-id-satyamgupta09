37091470284d79fe07ce93a5d24b502595affcf1
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-02-24 23:23:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function makeid(l) {
   // write your code here
+	const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+	let res="";
+
+	for(let i=0;i<l ;i++){
+		let index=Math.floor(Math.random()*chars.length);
+		res+=chars[index];
+	}
+
+	return res;
 }
 
 // Do not change the code below.
